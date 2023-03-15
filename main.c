55532cd21a47aa10ac3bfcffaf2da7a3ce2ee9b9
Anik Bepari <anik@gmail.com>
2023-03-15 17:05:44 +0530

    Push Feature 2

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
+git checkout feature2
+git merge master
+
 #include <stdio.h>
 
 int feature() {
   return 1;
 }
 
-feaature2()
-{
-  //Sample Code for Feature 2
+int feature2() {
+	return 3;
 }
 
-
+int security() {
+  return -1;
+}
 
 int main() {
-  int t = feature() + security();
+  int t = feature() + security() + feature2();
   printf("%d", t);
   
   return 0;
